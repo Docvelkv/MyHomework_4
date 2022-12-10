a690d59dc6251f13f061315be23bc5a988e6fdb6
Docvel <docvel@yandex.ru>
2022-12-10 04:07:29 +0300

    task25 solved

--- Task25/Program.cs
+++ Task25/Program.cs
@@ -1,2 +1,14 @@
 ﻿// Напишите цикл, который принимает на вход два числа (А и В)
 // и возводит число А в натуральную степень В.
+
+double NumNatPow (double a, int b) {
+    double res = 1;
+    for (int i = 1; i <= b; i++) {
+        res = a * res;}
+    return res;}
+
+Console.Clear();
+Console.WriteLine(NumNatPow(5, 4));
+Console.WriteLine(NumNatPow(7.5, 3));
+Console.WriteLine(NumNatPow(5, 0));
+Console.WriteLine(NumNatPow(-4.27, 7));
