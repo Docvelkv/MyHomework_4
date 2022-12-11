dc75bcb110a4aa29cfd0084e437ec40834e88244
Docvel <docvel@yandex.ru>
2022-12-11 06:13:54 +0300

    Task29 solved

--- Task27/Program.cs
+++ Task27/Program.cs
@@ -1,2 +1,18 @@
 ﻿// Напишите программу, которая принимает на вход число
 //  и выдаёт сумму цифр в числе
+
+long SumDigNum(long num) {
+    long sum = 0; 
+    long temp = 0;
+    if (num > 0) temp = num;
+    if (num < 0) temp = num * -1;
+    for (int i = 0; temp >= 1; i++) {
+        sum = sum + temp % 10;
+        temp = temp / 10;}
+    Console.Write($"Сумма цифр числа {num} - ");
+    return sum;}
+
+Console.Clear();
+Console.WriteLine(SumDigNum(25814764));
+Console.WriteLine(SumDigNum(-58246579));
+Console.WriteLine(SumDigNum(9876543210));
--- Task29/Program.cs
+++ Task29/Program.cs
@@ -1,3 +1,14 @@
 ﻿// Напишите программу, которая задаёт массив
-//  из 8 элементов и выводит их на экран
+//  из 8 элементов и выводит их на экран.
 
+void ArAnyRange(int n, int a, int z) {
+    int[] ar = new int[n];
+    for (int i = 0; i < n; i++) {
+        ar[i] = new Random().Next(a, z);}
+    Console.WriteLine($"Массив из {n} случайных элементов " +
+        $"в диапазоне от {a} до {z}: [{String.Join(", ", ar)}]");}
+    
+    Console.Clear();
+    ArAnyRange(8, 4258, 5879);
+    ArAnyRange(20, -400, 0);
+    ArAnyRange(12, 0, 400);
